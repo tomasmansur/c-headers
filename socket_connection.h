14805ab5628c27e1ec4388ffbdf3cd52d9a6346e
tomasmansur <61087373+tomasmansur@users.noreply.github.com>
2022-12-31 01:48:15 -0300

    Update socket_connection.h

--- socket_connection.h
+++ socket_connection.h
@@ -4,14 +4,14 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-int socketConnection(int fd,int serverPort)
+int socketConnection(int fd,char * address, int serverPort)
 {
     int eStatus;
     struct sockaddr_in remote;
     memset(&remote, 0, sizeof remote);
     
-    remote.sin_addr.s_addr = inet_addr("127.0.0.1");
     remote.sin_family = AF_INET;
+    remote.sin_addr.s_addr = inet_addr(address);
     remote.sin_port = htons(serverPort);
 
     eStatus = connect(fd, (struct sockaddr *) &remote, sizeof(struct sockaddr_in));
